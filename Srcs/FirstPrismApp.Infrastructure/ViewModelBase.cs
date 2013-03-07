a2423fca48f1c3220a1f5d6019c5ae69d8d8dab9
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-03-07 17:36:44 +0200

    Added ViewModelBase to Infrastructure proj

---
+++ Srcs/FirstPrismApp.Infrastructure/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace FirstPrismApp.Infrastructure
+{
+	public class ViewModelBase : IViewModel, INotifyPropertyChanged
+	{
+		public IView View { get; set; }
+
+		public ViewModelBase(IView view)
+		{
+			View = view;
+			View.ViewModel = this;
+		}
+
+		public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+		protected void RaisePropertyChanged(string prop)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(prop));
+			}
+		}
+	}
+}
