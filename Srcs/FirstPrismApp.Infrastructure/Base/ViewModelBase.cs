4726abd936fab369b640d08d1d9d6bc4897cdf83
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-03-15 16:40:44 +0200

    Changed ViewModelBase class , new method has been added, event was marked with NonSerialized property

--- Srcs/FirstPrismApp.Infrastructure/Base/ViewModelBase.cs
+++ Srcs/FirstPrismApp.Infrastructure/Base/ViewModelBase.cs
@@ -12,7 +12,8 @@ namespace FirstPrismApp.Infrastructure
 			if (View != null)
 				View.ViewModel = this;
 		}
-
+		
+		[field:System.NonSerialized]
 		public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
 
 		protected void RaisePropertyChanged(string prop)
@@ -20,6 +21,20 @@ namespace FirstPrismApp.Infrastructure
 			if (PropertyChanged != null)
 			{
 				PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(prop));
+			}
+		}
+
+		protected void RaisePropertiesChanged(params string[] propertyNames)
+		{
+			if (propertyNames == null)
+			{
+				throw new System.ArgumentNullException("propertyNames");
+			}
+			string[] strArray = propertyNames;
+			for (int i = 0; i < strArray.Length; i = (int)(i + 1))
+			{
+				string propertyName = strArray[i];
+				this.RaisePropertyChanged(propertyName);
 			}
 		}
 	}
